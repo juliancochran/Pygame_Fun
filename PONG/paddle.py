b6a78dbeaf0c6f37f098a5e4e4d9e28940308db1
Julian Cochran <julian.p.cochran@gmail.com>
2015-02-03 10:37:17 -0500

    Pong updates
    Finished Ball, Paddle and Pong game.

--- PONG/paddle.py
+++ PONG/paddle.py
@@ -1,23 +1,24 @@
 #!/bin/usr/env python2.7
-# ball.py
-# Object representation of a ball
-# 01/26/2015
+# paddle.py
+# Object representation of a paddle for Pong game
+# 01/30/2015
 __author__ = 'jcochran'
 
 import pygame
 from pygame import *
 
-class Ball:
+class Paddle:
     rect = None
     color = (255,255,255)
-    xs = 0
-    ys = 0
 
     def __init__(self, r):
         self.rect = r
 
-    def move(self):
-        self.rect.move_ip(self.xs, self.ys)
+    def moveUp(self):
+        self.rect.move_ip(0,-3)
+
+    def moveDown(self):
+        self.rect.move_ip(0,3)
 
     def colliding_with(self, other):
         return self.rect.colliderect(other)
